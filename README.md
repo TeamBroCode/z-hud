b2e823907844fd0d0234b68595a7c2065b40acec
Zemo <adityasingh.2888@gmail.com>
2023-12-20 16:21:26 +0530

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# qb-hud-buffs
+- A modification of qb-hud to handle the new [buffs system](https://github.com/tnj-development/tnj-buffs)
+
+
+`````````````````````MAKE SURE YOU RENAME THIS TO qb-hud       AND REPLACE IT WITH YOUR CURRENT HUD`````````````````````
+
+## Credits
+- QB-Hud comes from: [qb-hud](https://github.com/qbcore-framework/qb-hud) by [loljoshie](https://github.com/loljoshie)
+
---
+++ server.lua
@@ -0,0 +1,75 @@
+local QBCore = exports[Config.Core]:GetCoreObject()
+local ResetStress = false
+
+QBCore.Commands.Add('cash', 'Check Cash Balance', {}, false, function(source, args)
+    local Player = QBCore.Functions.GetPlayer(source)
+    local cashamount = Player.PlayerData.money.cash
+    TriggerClientEvent('hud:client:ShowAccounts', source, 'cash', cashamount)
+end)
+
+QBCore.Commands.Add('bank', 'Check Bank Balance', {}, false, function(source, args)
+    local Player = QBCore.Functions.GetPlayer(source)
+    local bankamount = Player.PlayerData.money.bank
+    TriggerClientEvent('hud:client:ShowAccounts', source, 'bank', bankamount)
+end)
+
+QBCore.Commands.Add("dev", "Enable/Disable developer Mode", {}, false, function(source, args)
+    TriggerClientEvent("qb-admin:client:ToggleDevmode", source)
+end, 'admin')
+
+RegisterNetEvent('hud:server:GainStress', function(amount)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local newStress
+    if not Player or (Config.DisablePoliceStress and Player.PlayerData.job.name == 'police') then return end
+    if not ResetStress then
+        if not Player.PlayerData.metadata['stress'] then
+            Player.PlayerData.metadata['stress'] = 0
+        end
+        newStress = Player.PlayerData.metadata['stress'] + amount
+        if newStress <= 0 then newStress = 0 end
+    else
+        newStress = 0
+    end
+    if newStress > 100 then
+        newStress = 100
+    end
+    Player.Functions.SetMetaData('stress', newStress)
+    TriggerClientEvent('hud:client:UpdateStress', src, newStress)
+    TriggerClientEvent('QBCore:Notify', src, Lang:t("notify.stress_gain"), 'error', 1500)
+end)
+
+RegisterNetEvent('hud:server:RelieveStress', function(amount)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local newStress
+    if not Player then return end
+    if not ResetStress then
+        if not Player.PlayerData.metadata['stress'] then
+            Player.PlayerData.metadata['stress'] = 0
+        end
+        newStress = Player.PlayerData.metadata['stress'] - amount
+        if newStress <= 0 then newStress = 0 end
+    else
+        newStress = 0
+    end
+    if newStress > 100 then
+        newStress = 100
+    end
+    Player.Functions.SetMetaData('stress', newStress)
+    TriggerClientEvent('hud:client:UpdateStress', src, newStress)
+    TriggerClientEvent('QBCore:Notify', src, Lang:t("notify.stress_removed"))
+end)
+
+QBCore.Functions.CreateCallback('hud:server:HasHarness', function(source, cb)
+    local Ply = QBCore.Functions.GetPlayer(source)
+    local Harness = Ply.Functions.GetItemByName("harness")
+    if Harness ~= nil then
+        cb(true)
+    else
+        cb(false)
+    end
+end)
+QBCore.Functions.CreateCallback('hud:server:getMenu', function(source, cb)
+    cb(Config.Menu)
+end) 
